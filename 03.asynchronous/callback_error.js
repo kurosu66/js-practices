d26f13fb24e4259213988ef068e0ad5340d75c29
kurosu <kurosu.66@gmail.com>
2025-06-12 21:49:56 +0900

    コールバックエラーあり実装

---
+++ 03.asynchronous/callback_error.js
@@ -0,0 +1,25 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
+
+db.parallelize(() => {
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+    () => {
+      db.run(
+        "INSERT INTO bookss (title) VALUES (?)",
+        ["アーサー王物語"],
+        function (err) {
+          if (err) {
+            console.log(`${err.message}`);
+          }
+          db.get("SELECT idd, title FROM books", (err) => {
+            if (err) {
+              console.log(`${err.message}`);
+            }
+            db.run("DROP TABLE IF EXISTS books");
+          });
+        },
+      );
+    },
+  );
+});
