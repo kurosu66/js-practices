73d710560877831f7c96c0530f76d0c476a94a5f
kurosu <kurosu.66@gmail.com>
2025-07-07 23:06:53 +0900

    parallelizeを使用しないよう変更

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -2,21 +2,19 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.parallelize(() => {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-    () => {
-      db.run(
-        "INSERT INTO books (title) VALUES (?)",
-        ["アーサー王物語"],
-        function () {
-          console.log(`${this.lastID}`);
-          db.get("SELECT id, title FROM books", (err, row) => {
-            console.log(`${row.id} ${row.title}`);
-            db.run("DROP TABLE IF EXISTS books");
-          });
-        },
-      );
-    },
-  );
-});
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run(
+      "INSERT INTO books (title) VALUES (?)",
+      ["アーサー王物語"],
+      function () {
+        console.log(`${this.lastID}`);
+        db.get("SELECT id, title FROM books", (err, row) => {
+          console.log(`${row.id} ${row.title}`);
+          db.run("DROP TABLE IF EXISTS books");
+        });
+      },
+    );
+  },
+);
