82e1c89193a9d0d5b5f3ddcfff9094587d14d3ab
kurosu <kurosu.66@gmail.com>
2025-08-06 07:36:31 +0900

    変数名修正

--- 03.asynchronous/async_await_no_error.js
+++ 03.asynchronous/async_await_no_error.js
@@ -4,12 +4,12 @@ await executeRunQuery(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
 );
-const insertMsg = await executeRunQuery(
+const result = await executeRunQuery(
   db,
   "INSERT INTO books (title) VALUES (?)",
   ["アーサー王物語"],
 );
-console.log(insertMsg.lastID);
-const getMsg = await executeGetQuery(db, "SELECT id, title FROM books");
-console.log(`${getMsg.id} ${getMsg.title}`);
+console.log(result.lastID);
+const row = await executeGetQuery(db, "SELECT id, title FROM books");
+console.log(`${row.id} ${row.title}`);
 await executeRunQuery(db, "DROP TABLE books");
