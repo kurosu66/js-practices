6f99bec7077825bc75d60f3389e4d319c9a74c15
kurosu66 <kurosu66@gmail.com>
2026-04-05 22:47:53 +0900

    dbをprivateに修正

--- 04.class/memo_database.js
+++ 04.class/memo_database.js
@@ -2,8 +2,10 @@ import { resolve } from "dns";
 import sqlite3 from "sqlite3";
 
 export default class MemoDatabase {
+  #db;
+
   constructor(dbPath) {
-    this.db = new sqlite3.Database(dbPath);
+    this.#db = new sqlite3.Database(dbPath);
   }
 
   async initializeDatabase() {
@@ -26,7 +28,7 @@ export default class MemoDatabase {
 
   #run(sql, params) {
     return new Promise((resolve, reject) => {
-      this.db.run(sql, params, function (err) {
+      this.#db.run(sql, params, function (err) {
         if (err) {
           reject(err);
         } else {
@@ -38,7 +40,7 @@ export default class MemoDatabase {
 
   #all(sql, params = []) {
     return new Promise((resolve, reject) => {
-      this.db.all(sql, params, (err, rows) => {
+      this.#db.all(sql, params, (err, rows) => {
         if (err) {
           reject(err);
         } else {
